b46ceb09bcca88afc95a9a3f4589d567a11dc399
swfairbourn <shawn.fairbourn@gmail.com>
2023-03-20 15:43:47 -0600

    Adding ID to Transaction

--- finance/src/main/java/com/fairbourn/finance/model/Transaction.java
+++ finance/src/main/java/com/fairbourn/finance/model/Transaction.java
@@ -6,17 +6,22 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Transaction {
 
+	private final UUID id;
 	private String merchant;
 	private double cost;
 	
 	
 	public Transaction(@JsonProperty("id") UUID id, 
-						@JsonProperty("merchant") String merchant, 
-						@JsonProperty("cost") double cost) {
+					   @JsonProperty("merchant") String merchant, 
+					   @JsonProperty("cost") double cost) {
+		this.id = id;
 		this.merchant = merchant;
 		this.cost = cost;
 	}
 
+	public UUID getId() {
+		return id;
+	}
 
 	public String getMerchant() {
 		return merchant;
